22620aab65b734f6f99a6aa21055538bf8355c83
Pedro Medina Márquez <medinamarquezp@gmail.com>
2022-02-18 01:09:19 +0100

    chore: config files

---
+++ src/config/coingecko.config.ts
@@ -0,0 +1,9 @@
+export const coingeckoConfig = {
+  basePath: 'https://api.coingecko.com/api/v3',
+  coinDataPath(coinId: string) {
+    return `${coingeckoConfig.basePath}/coins/${coinId}?localization=false&tickers=false&market_data=false&community_data=false&developer_data=false`;
+  },
+  coinMarketPath(top = 100, currency = 'usd', order = 'market_cap_desc') {
+    return `${coingeckoConfig.basePath}/coins/markets?vs_currency=${currency}&per_page=${top}&page=1&order=${order}`;
+  },
+};
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,14 @@
+import { join } from 'path';
 import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
 
 const env = process.env.NODE_ENV;
 const database = env === 'test' ? ':memory:' : 'data.db';
 const synchronize = env === 'production' ? false : true;
+const entities = [join(__dirname, '..', '**', '*.entity.{js,ts}')];
 
 export const typeormConfig: SqliteConnectionOptions = {
   type: 'sqlite',
-  entities: ['/src/**/*.entity.ts'],
+  entities,
   logging: false,
   database,
   synchronize,

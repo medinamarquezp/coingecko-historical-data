2490e71ed733c89f0fe9ac8d13c8d29478de5bc4
Pedro Medina Márquez <medinamarquezp@gmail.com>
2022-02-21 02:18:13 +0100

    feat: seed token command

--- src/commands/commands.console.ts
+++ src/commands/commands.console.ts
@@ -1,12 +1,37 @@
-import { Console, Command } from 'nestjs-console';
+import * as prettyMilliseconds from 'pretty-ms';
+import { Console, Command, createSpinner } from 'nestjs-console';
+import { TokensService } from 'src/tokens/services/tokens.service';
 
 @Console()
 export class CommandsConsole {
+  constructor(private readonly tokensService: TokensService) {}
+
   @Command({
     command: 'seed:token <tokenId>',
     description: 'Seed token data by token ID',
   })
   async seedTokenByID(tokenId: string): Promise<void> {
-    console.log(`Seeding token by ID ${tokenId}...`);
+    const initTimer = Date.now();
+    const spinner = createSpinner().start();
+    try {
+      spinner.info(`Seeding process started`);
+      spinner.info('Looking for token info on local storage');
+      const token = await this.tokensService.findToken(tokenId);
+      if (token) {
+        const { supplierId } = token;
+        spinner.info(`Seeding data of token "${supplierId}"`);
+        await this.tokensService.setToken(supplierId);
+      } else {
+        spinner.warn('No token info was found on local storage');
+        spinner.info(`Trying seeding data by using ID "${tokenId}"`);
+        await this.tokensService.setToken(tokenId);
+      }
+      const totalTime = prettyMilliseconds(Date.now() - initTimer);
+      spinner.succeed(`Seeding process finished successfully in ${totalTime}`);
+      spinner.stop();
+    } catch (error) {
+      spinner.fail(`Seeding procees fails: No token found by ID "${tokenId}"`);
+      spinner.stop();
+    }
   }
 }

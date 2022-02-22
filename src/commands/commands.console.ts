f16a9501b8d9e9e1133edb6e2bf9f0f7dfd89a5f
Pedro Medina Márquez <medinamarquezp@gmail.com>
2022-02-22 23:56:26 +0100

    feat: seed top command (v1)

--- src/commands/commands.console.ts
+++ src/commands/commands.console.ts
@@ -1,9 +1,13 @@
 import * as prettyMilliseconds from 'pretty-ms';
 import { Console, Command, createSpinner } from 'nestjs-console';
+import { sleep } from 'src/utils/sleep';
 import { TokensService } from 'src/tokens/services/tokens.service';
 
 @Console()
 export class CommandsConsole {
+  MAX_REQUEST_MIN = 50;
+  MIN_MILISECONDS = 60000;
+
   constructor(private readonly tokensService: TokensService) {}
 
   @Command({
@@ -12,7 +16,7 @@ export class CommandsConsole {
   })
   async seedTokenByID(tokenId: string): Promise<void> {
     const initTimer = Date.now();
-    const spinner = createSpinner().start();
+    const spinner = createSpinner();
     try {
       spinner.info(`Seeding process started`);
       spinner.info('Looking for token info on local storage');
@@ -31,6 +35,60 @@ export class CommandsConsole {
       spinner.stop();
     } catch (error) {
       spinner.fail(`Seeding procees fails: No token found by ID "${tokenId}"`);
+      spinner.stop();
+    }
+  }
+
+  @Command({
+    command: 'seed:top <top>',
+    description: 'Seed tokens by top market cap',
+  })
+  async seedTokensByTop(top: number): Promise<void> {
+    const initTimer = Date.now();
+    const spinner = createSpinner();
+    try {
+      spinner.info(`Seeding process started`);
+      spinner.info('Getting total active tokens');
+      const totalTokens = await this.tokensService.getTotalActiveTokens();
+
+      if (top <= 0 || top > totalTokens) {
+        throw new Error('Top parameter is out of bounds');
+      }
+
+      let page = 1;
+      const perPage = 50;
+      const numPages = Math.ceil(top / this.MAX_REQUEST_MIN);
+      if (numPages > 1) {
+        spinner.warn(`Seeding process will take about ${numPages} minutes...`);
+      }
+
+      // TODO:
+      // Coingecko free API has a limitation of 50 RQs in a minute.
+      // Temporarily we will processing in batch of 50 RQs doing pauses every minute.
+      // It will be necessary to move this logic to a queue in order to process it in background.
+      // In addition, it would only be necessary to download those tokens that do not exist
+      // or whose date of last update is greater than a week.
+      while (page <= numPages) {
+        spinner.start(`Processing iteration ${page}`);
+        await this.tokensService.setTopMarket(page, perPage);
+        spinner.stop();
+        spinner.succeed(`Iteration ${page} processed successfully`);
+        spinner.start(
+          `Iteration ${page} of ${numPages}. Next iteration will be processed in ${prettyMilliseconds(
+            this.MIN_MILISECONDS,
+          )}, be patient...`,
+        );
+        if (page === numPages) break;
+        await sleep(this.MIN_MILISECONDS);
+        spinner.stop();
+        page += 1;
+      }
+
+      const totalTime = prettyMilliseconds(Date.now() - initTimer);
+      spinner.succeed(`Seeding process finished successfully in ${totalTime}`);
+      spinner.stop();
+    } catch (error) {
+      spinner.fail(`Seeding procees fails: "${error.message}"`);
       spinner.stop();
     }
   }
